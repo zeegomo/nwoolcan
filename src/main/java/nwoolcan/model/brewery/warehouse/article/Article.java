ddbd52b2f0413fe33b17f0508e2ef77fd264d8f3
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-02-15 23:22:17 +0100

    finish ArticleType interface implementation

--- src/main/java/nwoolcan/model/brewery/warehouse/article/Article.java
+++ src/main/java/nwoolcan/model/brewery/warehouse/article/Article.java
@@ -1,5 +1,7 @@
 package nwoolcan.model.brewery.warehouse.article;
 
+import nwoolcan.utils.Result;
+
 /**
  * Article.
  */
@@ -9,4 +11,28 @@ public interface Article {
      * @return the Id of the current article.
      */
     Integer getId();
+
+    /**
+     * Returns the {@link ArticleType} linked to the Article.
+     * @return the {@link ArticleType} linked to the current Article.
+     */
+    ArticleType getArticleType();
+
+    /**
+     * Returns the denomination of the current Article.
+     * @return the denomination of the current Article.
+     */
+    String getName();
+
+    /**
+     * If the current {@link ArticleType} is {@link ArticleType}.INGREDIENT returns the casted object.
+     * @return a {@link Result} of {@link IngredientArticle} casting the current Article.
+     */
+    Result<IngredientArticle> toIngredientArticle();
+
+    /**
+     * If the current {@link ArticleType} is {@link ArticleType}.FINISHED_BEER returns the casted object.
+     * @return a {@link Result} of {@link BeerArticle} casting the current Article.
+     */
+    Result<BeerArticle> toBeerArticle();
 }
