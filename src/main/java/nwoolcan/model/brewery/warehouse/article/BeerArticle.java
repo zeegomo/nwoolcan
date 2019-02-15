922568d028467b5e2472119be1daf7f05633cf92
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-02-15 23:28:24 +0100

    finish BeerArticle interface implementation

--- src/main/java/nwoolcan/model/brewery/warehouse/article/BeerArticle.java
+++ src/main/java/nwoolcan/model/brewery/warehouse/article/BeerArticle.java
@@ -1,7 +1,14 @@
 package nwoolcan.model.brewery.warehouse.article;
 
+import nwoolcan.model.brewery.production.batch.Batch;
+
 /**
  * BeerArticle.
  */
 public interface BeerArticle extends Article {
+    /**
+     * Returns the Batch linked to this BeerArticle.
+     * @return the Batch linked to this BeerArticle.
+     */
+    Batch getBatch();
 }
