95a311887248c944d5a41e6569983023c376ca1d
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-02-15 12:47:12 +0100

    implement Quantity

--- src/main/java/nwoolcan/model/utils/Quantity.java
+++ src/main/java/nwoolcan/model/utils/Quantity.java
@@ -1,7 +1,18 @@
 package nwoolcan.model.utils;
 
 /**
- * Quantity.
+ * Quantity interface for handling value and unit of measure.
  */
 public interface Quantity {
+    /**
+     * Returns the quantity value.
+     * @return quantity value.
+     */
+    Number getValue();
+
+    /**
+     * Returns the quantity unit of measure.
+     * @return quantity unit of measure.
+     */
+    UnitOfMeasure getUnitOfMeasure();
 }
