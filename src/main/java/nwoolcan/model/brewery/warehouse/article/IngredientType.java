f5ae192b1c9e1145a566660e53a838a6d78fd161
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-02-15 23:37:46 +0100

    add IngredientType Enum

---
+++ src/main/java/nwoolcan/model/brewery/warehouse/article/IngredientType.java
@@ -0,0 +1,11 @@
+package nwoolcan.model.brewery.warehouse.article;
+
+/**
+ * The Type of {@link IngredientArticle} which can be stored in the warehouse.
+ */
+public enum IngredientType {
+    /**
+     * The warehouse can store these type of ingredients: Hops, Fermentables, Yeasts and others.
+     */
+    HOPS, FERMENTABLE, YEAST, OTHER;
+}
