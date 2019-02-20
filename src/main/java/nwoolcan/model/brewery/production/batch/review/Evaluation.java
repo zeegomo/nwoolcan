722c56c6d44252ebee8490fdce519bbc04c7bd07
Giacomo Pasini <g.pasini98@gmail.com>
2019-02-20 20:30:29 +0100

    define evaluation methods

--- src/main/java/nwoolcan/model/brewery/production/batch/review/Evaluation.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/review/Evaluation.java
@@ -1,7 +1,24 @@
 package nwoolcan.model.brewery.production.batch.review;
 
+import java.util.Optional;
+
 /**
  * Evaluation.
  */
 public interface Evaluation {
+    /**
+     * Returns the score.
+     * @return the score.
+     */
+    int getScore();
+    /**
+     * Returns the maximum possible score.
+     * @return the maximum possible score.
+     */
+    int getMaxScore();
+    /**
+     * Returns evaluation notes, if any.
+     * @return evaluation notes, if any.
+     */
+    Optional<String> getNotes();
 }
