43971a6951494827730a9e53a1c5ceb5b76343c3
Giacomo Pasini <g.pasini98@gmail.com>
2019-02-20 20:32:25 +0100

    BatchReview methods definition

--- src/main/java/nwoolcan/model/brewery/production/batch/review/BatchReview.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/review/BatchReview.java
@@ -1,7 +1,29 @@
 package nwoolcan.model.brewery.production.batch.review;
 
+import java.util.Collection;
+
 /**
  * BatchReview.
  */
-public interface BatchReview {
+public interface BatchReview extends Evaluation {
+    /**
+     * Return an identifier of the review type, like "Official BJCP Review".
+      * @return an identifier of the review type
+     */
+    String getName();
+    /**
+     * Return an identifier of the reviewer, like "John Doe".
+     * @return an identifier of the reviewer.
+     */
+    String getReviewer();
+    /**
+     * Returns the category evaluations.
+     * @return the category evaluations.
+     */
+    Collection<CategoryEvaluation> getCategoryEvaluations();
+    /**
+     * List all categories.
+     * @return the category list.
+     */
+    Collection<CategoryEvaluationType> getCategories();
 }
