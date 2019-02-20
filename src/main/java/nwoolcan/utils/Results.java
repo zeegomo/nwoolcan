4ce248fefbbe4678739d6eccbc6caf020951223f
Giacomo Pasini <g.pasini98@gmail.com>
2019-02-20 17:52:44 +0100

    add Results utilities

---
+++ src/main/java/nwoolcan/utils/Results.java
@@ -0,0 +1,24 @@
+package nwoolcan.utils;
+
+/**
+ * Utilites for Result.
+ */
+public final class Results {
+
+    private Results() { }
+
+    /**
+     * Returns a {@link Result} holding elem if elem is not null, otherwise holds a NullPointerException.
+     * @param elem the elem to be tested
+     * @param <T> elem type
+     * @return a {@link Result} holding elem if elem is not null, otherwise holds a NullPointerException
+     */
+    public static <T> Result<T> requireNonNull(T elem) {
+        if (elem == null) {
+            return Result.error(new NullPointerException());
+        } else {
+            return Result.of(elem);
+        }
+    }
+
+}
