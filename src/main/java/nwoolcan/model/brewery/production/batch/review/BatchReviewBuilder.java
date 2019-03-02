92490d56d4137afa098c8bb9462d7ec3622fbc9d
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-02 18:56:42 +0100

    add support for enum implementations of BatchReviewType

--- src/main/java/nwoolcan/model/brewery/production/batch/review/BatchReviewBuilder.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/review/BatchReviewBuilder.java
@@ -5,11 +5,9 @@ import io.github.classgraph.ClassInfoList;
 import nwoolcan.utils.Result;
 import nwoolcan.utils.Results;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * {@link BatchReview} builder.
@@ -17,10 +15,15 @@ import java.util.stream.Collectors;
  */
 
 public final class BatchReviewBuilder {
+    private static final String INVALID_SCORE_MESSAGE = "Score for one or more categories is not valid";
+    private static final String INVALID_CATEGORIES_MESSAGE = "Invalid categories";
+    private static final String BUILDER_USED_MESSAGE = "This builder has already built";
+
     private final Set<Evaluation> evaluations = new HashSet<>();
     private String reviewer;
     private String notes;
-    private final BatchReviewType batchReviewType;
+    private boolean built;
+    private BatchReviewType batchReviewType;
 
     /**
      * Return a collection of all available {@link BatchReviewType} types.
@@ -30,10 +33,17 @@ public final class BatchReviewBuilder {
     public static Result<Collection<BatchReviewType>> getAvailableBatchReviewTypes() {
         return Results.ofCloseable(() ->  new ClassGraph().enableAllInfo().scan(), scanResult -> {
             ClassInfoList widgetClasses = scanResult.getClassesImplementing(BatchReviewType.class.getName());
+            System.out.println(widgetClasses.size());
             return widgetClasses
                 .loadClasses(BatchReviewType.class)
                 .stream()
-                .map(review -> Results.ofChecked(review::newInstance))
+                .flatMap(review -> {
+                    if (review.isEnum()) {
+                        return Arrays.stream(review.getEnumConstants()).map(Result::of);
+                    } else {
+                        return Stream.of(Results.ofChecked(review::newInstance));
+                    }
+                })
                 .filter(Result::isPresent)
                 .map(Result::getValue)
                 .collect(Collectors.toList());
@@ -43,10 +53,11 @@ public final class BatchReviewBuilder {
     /**
      * Create a {@link BatchReviewBuilder}.
      *
-     * @param batchReviewType the {@link BatchReviewType} of this {@link BatchReview} (implements the Strategy pattern)
+     * @param batchReviewType the {@link BatchReviewType} of this {@link BatchReview}
+     *                        (implements the Strategy pattern)
      */
     public BatchReviewBuilder(final BatchReviewType batchReviewType) {
-        this.batchReviewType = batchReviewType;
+        reset(batchReviewType);
     }
 
     /**
@@ -97,17 +108,28 @@ public final class BatchReviewBuilder {
     }
 
     /**
-     * Return a {@link Result} holding a {@link BatchReview} if everything went well, otherwise a {@link Result} holding an {@link Exception}.
+     * Return whether this builder is already built.
+     * @return whether this builder is already built.
+     */
+    public boolean isBuilt() {
+        return this.built;
+    }
+
+    /**
+     * Return a {@link Result} holding a {@link BatchReview} if everything went well, otherwise a
+     * {@link Result} holding an {@link Exception}.
      *
      * @return a {@link Result<BatchReview>}
      */
     public Result<BatchReview> build() {
         return Result.of(this)
-            .require(BatchReviewBuilder::checkEvaluationsTypeValidity)
-            .require(builder -> builder.evaluations
-                .stream()
-                .allMatch(cat -> cat.getScore() >= 0
-                    && cat.getScore() <= cat.getEvaluationType().getMaxScore()))
+            .require(BatchReviewBuilder::checkEvaluationsTypeValidity,
+                new IllegalArgumentException(INVALID_CATEGORIES_MESSAGE))
+            .require(BatchReviewBuilder::checkScoreValidity,
+                new IllegalAccessException(INVALID_SCORE_MESSAGE))
+            .require(builder -> !builder.built,
+                new IllegalStateException(BUILDER_USED_MESSAGE))
+            .peek(builder -> builder.built = true)
             .map(builder -> new BatchReviewImpl(builder.batchReviewType,
                 builder.evaluations,
                 Optional.ofNullable(builder.reviewer),
@@ -115,14 +137,17 @@ public final class BatchReviewBuilder {
     }
 
     /**
-     * Resets the builder but not the {@link BatchReviewType}.
+     * Resets the builder.
      *
+     * @param newType the {@link BatchReviewType} of the new review.
      * @return this
      */
-    public BatchReviewBuilder reset() {
+    public BatchReviewBuilder reset(final BatchReviewType newType) {
         this.evaluations.clear();
         this.notes = null;
         this.reviewer = null;
+        this.built = false;
+        this.batchReviewType = newType;
         return this;
     }
 
@@ -133,5 +158,12 @@ public final class BatchReviewBuilder {
             .collect(Collectors.toList());
         return catTypes.containsAll(this.batchReviewType.getCategories())
             && this.batchReviewType.getCategories().containsAll(catTypes);
+    }
+
+    private boolean checkScoreValidity() {
+        return this.evaluations
+            .stream()
+            .allMatch(cat -> cat.getScore() >= 0
+                && cat.getScore() <= cat.getEvaluationType().getMaxScore());
     }
 }
