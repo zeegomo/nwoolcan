8c2916a647bdd97e9e32ad511bd5d17ee128e8eb
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-03 17:41:21 +0100

    add lazy initialization

--- src/main/java/nwoolcan/model/brewery/production/batch/review/BatchReviewScannerImpl.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/review/BatchReviewScannerImpl.java
@@ -14,9 +14,20 @@ import java.util.stream.Stream;
  * Implementation for BatchReviewScanner.
  */
 public final class BatchReviewScannerImpl implements BatchReviewScanner {
+    private Collection<BatchReviewType> types;
 
     @Override
     public Result<Collection<BatchReviewType>> getAvailableBatchReviewTypes() {
+        if (types != null) {
+            return Result.of(types);
+        } else {
+            return this.scan()
+                .peek(res -> this.types = res);
+        }
+    }
+
+
+    private Result<Collection<BatchReviewType>> scan() {
         return Results.ofCloseable(() ->  new ClassGraph().enableAllInfo().scan(), scanResult -> {
             ClassInfoList widgetClasses = scanResult.getClassesImplementing(BatchReviewType.class.getName());
             return widgetClasses
