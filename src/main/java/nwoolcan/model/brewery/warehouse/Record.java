da4c9457e7f2c0edf1330cd2f56ecae2ed73421f
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-03-04 16:39:39 +0100

    add Action enum to Record and its implementation

--- src/main/java/nwoolcan/model/brewery/warehouse/Record.java
+++ src/main/java/nwoolcan/model/brewery/warehouse/Record.java
@@ -9,40 +9,63 @@ import java.util.Objects;
  * Class which keeps track of a {@link Quantity} and a {@link Date}.
  */
 public final class Record {
+    /**
+     * Defines if the record is positive or negative.
+     */
+    public enum Action {
+        /**
+         * If the record Action is USING the quantity will be subtracted,
+         * while it will be added if the Action is ADDING.
+         */
+        USING, ADDING
+    }
 
     private final Quantity quantity;
     private final Date date;
+    private final Action action;
+
+
+
 
     /**
      * Constructor with all parameters.
      * @param quantity to be recorded.
      * @param date in which the recorded happened.
+     * @param action which states if quantity is added or subtracted.
      */
-    public Record(final Quantity quantity, final Date date) {
+    public Record(final Quantity quantity, final Date date, final Action action) {
         this.quantity = Objects.requireNonNull(quantity);
         this.date = Objects.requireNonNull(date);
+        this.action = Objects.requireNonNull(action);
     }
     /**
      * Constructor which auto instantiate the current {@link Date}.
      * @param quantity to be recorded.
+     * @param action which states if quantity is added or subtracted.
      */
-    public Record(final Quantity quantity) {
-        this.quantity = quantity;
-        this.date = new Date();
+    public Record(final Quantity quantity, final Action action) {
+        this(quantity, new Date(), action);
     }
     /**
      * Getter for the date.
      * @return the date of the record.
      */
-    Date getDate() {
+    public Date getDate() {
         return (Date) this.date.clone();
     }
     /**
-     * Getter for the quantity.
+     * Getter for the quantity. It is not protected since it does not have any setter.
      * @return the quantity of the record.
      */
-    Quantity getQuantity() {
+    public Quantity getQuantity() {
         return this.quantity;
+    }
+    /**
+     * Getter for the quantity.
+     * @return the action of the current record.
+     */
+    public Action getAction() {
+        return this.action;
     }
 
 }
