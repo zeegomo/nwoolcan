5354622788717805aa837acc4dd10abed3903fa0
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-03-08 12:45:11 +0100

    add BreweryImpl class init implementation

--- src/main/java/nwoolcan/model/brewery/BreweryImpl.java
+++ src/main/java/nwoolcan/model/brewery/BreweryImpl.java
@@ -1,7 +1,34 @@
 package nwoolcan.model.brewery;
 
+import nwoolcan.model.brewery.production.batch.Batch;
+import nwoolcan.model.brewery.production.batch.BatchInfo;
+import nwoolcan.model.brewery.warehouse.Warehouse;
+import nwoolcan.utils.Empty;
+import nwoolcan.utils.Result;
+
+import java.util.Collection;
+
 /**
  * Brewery Implementation.
  */
-public class BreweryImpl implements Brewery {
+public final class BreweryImpl implements Brewery {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Warehouse getWarehouse() {
+        return null;
+    }
+
+    @Override
+    public Result<Collection<Batch>> getBatches(final QueryBatch queryBatch) {
+        return null;
+    }
+
+    @Override
+    public Result<Empty> createBatch(final BatchInfo batchInfo) {
+        return null;
+    }
 }
