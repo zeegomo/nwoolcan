68dd49a40fc672bfd9be63d0fce41757ca89dac0
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-18 21:23:32 +0100

    add peekError function

--- src/main/java/nwoolcan/utils/function/FallibleRunnable.java
+++ src/main/java/nwoolcan/utils/function/FallibleRunnable.java
@@ -2,12 +2,13 @@ package nwoolcan.utils.function;
 
 /**
  * Represent a function with no parameter and no result that may throw an error.
+ * @param <E> type of exception
  */
 @FunctionalInterface
-public interface FallibleRunnable {
+public interface FallibleRunnable<E extends Exception> {
     /**
      * Run the function.
-     * @throws Exception the exception to be thrown
+     * @throws E the exception to be thrown.
      */
-    void run() throws Exception;
+    void run() throws E;
 }
