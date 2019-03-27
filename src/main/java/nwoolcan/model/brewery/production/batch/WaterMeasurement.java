2f2725a5927fb62fdcb27586db9a83962eb53063
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-27 11:08:12 +0100

    modify javadoc

--- src/main/java/nwoolcan/model/brewery/production/batch/WaterMeasurement.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/WaterMeasurement.java
@@ -1,6 +1,6 @@
 package nwoolcan.model.brewery.production.batch;
 
-import nwoolcan.model.utils.Quantity;
+import nwoolcan.model.brewery.production.batch.step.parameter.Parameter;
 
 import java.util.Optional;
 
@@ -9,33 +9,33 @@ import java.util.Optional;
  */
 public interface WaterMeasurement {
         /**
-         * Returns the amount of Ca+2.
-         * @return a {@link Quantity} describing Ca+2 levels
+         * Returns the amount of Ca+2 in mg/L.
+         * @return a {@link Parameter} describing Ca+2 levels
          */
-        Optional<Quantity> getCalciumIons();
+        Optional<Parameter> getCalciumIons();
         /**
-         * Returns the amount of Mg+2.
-         * @return a {@link Quantity} describing Mg+2 levels
+         * Returns the amount of Mg+2 in mg/L.
+         * @return a {@link Parameter} describing Mg+2 levels
          */
-        Optional<Quantity> getMagnesiumIons();
+        Optional<Parameter> getMagnesiumIons();
         /**
-         * Returns the amount of Na+1.
-         * @return a {@link Quantity} describing Na+1 levels
+         * Returns the amount of Na+1 in mg/L.
+         * @return a {@link Parameter} describing Na+1 levels
          */
-        Optional<Quantity> getSodiumIons();
+        Optional<Parameter> getSodiumIons();
         /**
-         * Returns the amount of HCO3-1.
-         * @return a {@link Quantity} describing HCO3-1 levels
+         * Returns the amount of HCO3-1 in mg/L.
+         * @return a {@link Parameter} describing HCO3-1 levels
          */
-        Optional<Quantity> getBicarbonateIons();
+        Optional<Parameter> getBicarbonateIons();
         /**
-         * Returns the amount of SO4-2.
-         * @return a {@link Quantity} describing SO4-2 levels
+         * Returns the amount of SO4-2 in mg/L.
+         * @return a {@link Parameter} describing SO4-2 levels
          */
-        Optional<Quantity> getSulfateIons();
+        Optional<Parameter> getSulfateIons();
         /**
-         * Returns the amount of Cl-1.
-         * @return a {@link Quantity} describing Cl-1 levels
+         * Returns the amount of Cl-1 in mg/L.
+         * @return a {@link Parameter} describing Cl-1 levels
          */
-        Optional<Quantity> getChlorideIons();
+        Optional<Parameter> getChlorideIons();
 }
