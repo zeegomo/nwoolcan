283af629c812e42977334c57c82add0a4fd7a6a7
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-27 11:12:40 +0100

    add watermeasurementsimpl and builder and relevant tests

---
+++ src/main/java/nwoolcan/model/brewery/production/batch/WaterMeasurementBuilder.java
@@ -0,0 +1,136 @@
+package nwoolcan.model.brewery.production.batch;
+
+import nwoolcan.model.brewery.production.batch.step.parameter.Parameter;
+import nwoolcan.model.brewery.production.batch.step.parameter.ParameterTypeEnum;
+import nwoolcan.utils.Result;
+
+import java.util.EnumMap;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Water measurement implementation.
+ */
+public class WaterMeasurementBuilder {
+    private static final String INVALID_PARAMETER = "One or more parameter are not a valid water measurement type";
+    private static final String BUILDER_BUILT = "This builder has already built";
+    private enum Ions {
+        CALCIUM, MAGNESIUM, SODIUM, BICARBONATE, SULFATE, CHLORIDE
+    }
+
+    private final EnumMap<Ions, Parameter> parameter = new EnumMap<>(Ions.class);
+    private boolean built;
+
+    /**
+     * Add Calcium measurement.
+     * @param p the parameter holding the measure.
+     * @return this.
+     */
+    public WaterMeasurementBuilder addCalciumIonsRegistration(final Parameter p) {
+        this.parameter.put(Ions.CALCIUM, p);
+        return this;
+    }
+    /**
+     * Add Calcium measurement.
+     * @param p the parameter holding the measure.
+     * @return this.
+     */
+    public WaterMeasurementBuilder addMagnesiumIonsRegistration(final Parameter p) {
+        this.parameter.put(Ions.MAGNESIUM, p);
+        return this;
+    }
+    /**
+     * Add Calcium measurement.
+     * @param p the parameter holding the measure.
+     * @return this.
+     */
+    public WaterMeasurementBuilder addSodiumIonsRegistration(final Parameter p) {
+        this.parameter.put(Ions.SODIUM, p);
+        return this;
+    }
+    /**
+     * Add Calcium measurement.
+     * @param p the parameter holding the measure.
+     * @return this.
+     */
+    public WaterMeasurementBuilder addSulfateIonsRegistration(final Parameter p) {
+        this.parameter.put(Ions.SULFATE, p);
+        return this;
+    }
+    /**
+     * Build the water measurement object.
+     * @return a {@link Result} holding a {@link WaterMeasurement} if everything went well.
+     */
+    public Result<WaterMeasurement> build() {
+        return Result.of(this.parameter)
+                     .require(params -> params.values().stream().allMatch(p -> p.getType().equals(ParameterTypeEnum.WATER_MEASUREMENT)),
+                         new IllegalArgumentException(INVALID_PARAMETER))
+                     .require(() -> this.built, new IllegalStateException(BUILDER_BUILT))
+                     .peek(params -> this.built = true)
+                     .map(WaterMeasurementImpl::new);
+    }
+    /**
+     * Reset the builder.
+     * @return this.
+     */
+    public WaterMeasurementBuilder reset() {
+        this.built = false;
+        this.parameter.clear();
+        return this;
+    }
+
+    private static final class WaterMeasurementImpl implements WaterMeasurement {
+        private final EnumMap<Ions, Parameter> parameter;
+
+        private WaterMeasurementImpl(final EnumMap<Ions, Parameter> parameters) {
+            this.parameter = parameters;
+        }
+
+        @Override
+        public Optional<Parameter> getCalciumIons() {
+            return Optional.ofNullable(parameter.get(Ions.CALCIUM));
+        }
+
+        @Override
+        public Optional<Parameter> getMagnesiumIons() {
+            return Optional.ofNullable(parameter.get(Ions.MAGNESIUM));
+        }
+
+        @Override
+        public Optional<Parameter> getSodiumIons() {
+            return Optional.ofNullable(parameter.get(Ions.SODIUM));
+        }
+
+        @Override
+        public Optional<Parameter> getBicarbonateIons() {
+            return Optional.ofNullable(parameter.get(Ions.BICARBONATE));
+        }
+
+        @Override
+        public Optional<Parameter> getSulfateIons() {
+            return Optional.ofNullable(parameter.get(Ions.SULFATE));
+        }
+
+        @Override
+        public Optional<Parameter> getChlorideIons() {
+            return Optional.ofNullable(parameter.get(Ions.CHLORIDE));
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            WaterMeasurementImpl that = (WaterMeasurementImpl) o;
+            return Objects.equals(parameter, that.parameter);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(parameter);
+        }
+    }
+}
---
+++ src/test/java/nwoolcan/model/brewery/production/batch/WaterMeasurementBuilderTest.java
@@ -0,0 +1,55 @@
+package nwoolcan.model.brewery.production.batch;
+
+import nwoolcan.model.brewery.production.batch.step.parameter.Parameter;
+import nwoolcan.model.brewery.production.batch.step.parameter.ParameterImpl;
+import nwoolcan.model.brewery.production.batch.step.parameter.ParameterTypeEnum;
+import nwoolcan.utils.Result;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for {@link WaterMeasurementBuilder}.
+ */
+public class WaterMeasurementBuilderTest {
+    /**
+     * test successful build.
+     */
+    @Test
+    public void testSuccessfulBuild() {
+        final int value = 15;
+        WaterMeasurementBuilder builder = new WaterMeasurementBuilder().addCalciumIonsRegistration(new ParameterImpl(ParameterTypeEnum.WATER_MEASUREMENT, value));
+        Result<WaterMeasurement> water = builder.build();
+        assertTrue(water.isPresent());
+        assertFalse(water.getValue().getBicarbonateIons().isPresent());
+        assertEquals(water.getValue().getCalciumIons().get().getRegistrationValue(), value);
+
+        Result<WaterMeasurement> water2 = builder.reset()
+                                                 .addMagnesiumIonsRegistration(new ParameterImpl(ParameterTypeEnum.WATER_MEASUREMENT, value))
+                                                 .addCalciumIonsRegistration(new ParameterImpl(ParameterTypeEnum.WATER_MEASUREMENT, 1))
+                                                 .addSodiumIonsRegistration(new ParameterImpl(ParameterTypeEnum.WATER_MEASUREMENT, 0))
+                                                 .build();
+        assertTrue(water2.isPresent());
+        assertFalse(water2.getValue().getBicarbonateIons().isPresent());
+        assertEquals(water.getValue().getMagnesiumIons().get().getRegistrationValue(), value);
+        assertEquals(water.getValue().getCalciumIons().get().getRegistrationValue(), 1);
+        assertEquals(water.getValue().getSodiumIons().get().getRegistrationValue(), 0);
+    }
+    /**
+     * test successful build.
+     */
+    @Test
+    public void testFailedBuild() {
+        final int value = 15;
+        WaterMeasurementBuilder builder = new WaterMeasurementBuilder().addCalciumIonsRegistration(new ParameterImpl(ParameterTypeEnum.ABV, value));
+        Result<WaterMeasurement> water = builder.build();
+        assertTrue(water.isError());
+
+        Result<WaterMeasurement> water1 =  builder.reset().build();
+        assertTrue(water1.isPresent());
+        assertFalse(water1.getValue().getChlorideIons().isPresent());
+
+        Result<WaterMeasurement> water2 = builder.build();
+        assertTrue(water2.isError());
+    }
+}
