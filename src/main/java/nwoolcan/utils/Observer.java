e4982f4856cf09aa8947f019c35c96e0ad0d1262
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-27 11:10:46 +0100

    add Observer interface

--- src/main/java/nwoolcan/utils/Observer.java
+++ src/main/java/nwoolcan/utils/Observer.java
@@ -1,6 +1,14 @@
 package nwoolcan.utils;
 
+/**
+ * Observer pattern.
+ * @param <T> the type of objects
+ */
 @FunctionalInterface
 public interface Observer<T> {
+    /**
+     * This method is called whenever the observed object is changed.
+     * @param arg an argument passed.
+     */
     void update(T arg);
 }
