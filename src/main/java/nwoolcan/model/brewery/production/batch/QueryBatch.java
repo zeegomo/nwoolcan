f7943b528780e9b045755f3eccc29f13039a1807
Samuele Ceroni <samuele.ceroni@gmail.com>
2019-03-29 18:16:30 +0100

    implement queryBatch

--- src/main/java/nwoolcan/model/brewery/production/batch/QueryBatch.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/QueryBatch.java
@@ -1,8 +1,55 @@
 package nwoolcan.model.brewery.production.batch;
 
+import nwoolcan.model.utils.Quantity;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+
 /**
  * Query Batch.
  */
 public class QueryBatch {
+
+    @Nullable private Integer minId;
+    @Nullable private Integer maxId;
+    @Nullable private BatchMethod batchMethod;
+    @Nullable private Quantity minBatchSize;
+    @Nullable private Quantity maxBatchSize;
+
+    /**
+     * Return an int denoting the min id to be displayed.
+     * @return an int denoting the min id to be displayed.
+     */
+    public Optional<Integer> getMinId() {
+        return Optional.ofNullable(minId);
+    }
+    /**
+     * Return an int denoting the max id to be displayed.
+     * @return an int denoting the max id to be displayed.
+     */
+    public Optional<Integer> getMaxId() {
+        return Optional.ofNullable(maxId);
+    }
+    /**
+     * Return the only {@link BatchMethod} to be displayed.
+     * @return the only {@link BatchMethod} to be displayed.
+     */
+    public Optional<BatchMethod> getBatchMethod() {
+        return Optional.ofNullable(batchMethod);
+    }
+    /**
+     * Return a {@link Quantity} denoting the minimum size of the {@link Batch}.
+     * @return a {@link Quantity} denoting the minimum size of the {@link Batch}.
+     */
+    public Optional<Quantity> getMinBatchSize() {
+        return Optional.ofNullable(minBatchSize);
+    }
+    /**
+     * Return a {@link Quantity} denoting the maximum size of the {@link Batch}.
+     * @return a {@link Quantity} denoting the maximum size of the {@link Batch}.
+     */
+    public Optional<Quantity> getMaxBatchSize() {
+        return Optional.ofNullable(maxBatchSize);
+    }
 
 }
