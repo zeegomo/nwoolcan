b3ec3ee7b9bf9835707506422cbcaaf4c18d4f55
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-29 15:35:44 +0100

    fix warning using new ArrayList<>() insteand of Collections.emptyList for type inference

--- src/test/java/nwoolcan/model/brewery/warehouse/article/TestBeerArticle.java
+++ src/test/java/nwoolcan/model/brewery/warehouse/article/TestBeerArticle.java
@@ -10,7 +10,7 @@ import nwoolcan.model.utils.UnitOfMeasure;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Collections;
+import java.util.ArrayList;
 
 /**
  * Test for BeerArticle.
@@ -25,7 +25,7 @@ public class TestBeerArticle {
         new BeerDescriptionImpl("Test beer", "Test style"),
         BatchMethod.ALL_GRAIN,
         Quantity.of(1000, UnitOfMeasure.MILLILITER),
-        Collections.EMPTY_LIST,
+        new ArrayList<>(),
         StepTypeEnum.MASHING
     );
 
