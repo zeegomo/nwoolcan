f1222be48761b83fea8519a265cbc55146c030cc
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-29 16:45:51 +0100

    add factory for ModifiableBatchInfo

---
+++ src/main/java/nwoolcan/model/brewery/production/batch/ModifiableBatchInfoFactory.java
@@ -0,0 +1,31 @@
+package nwoolcan.model.brewery.production.batch;
+
+import javafx.util.Pair;
+import nwoolcan.model.brewery.production.batch.misc.BeerDescription;
+import nwoolcan.model.brewery.production.batch.misc.WaterMeasurement;
+import nwoolcan.model.brewery.warehouse.article.IngredientArticle;
+import nwoolcan.model.utils.Quantity;
+
+import java.util.Collection;
+
+/**
+ * Constructs a {@link ModifiableBatchInfo}
+ */
+final class ModifiableBatchInfoFactory {
+
+    static ModifiableBatchInfo of(final Collection<Pair<IngredientArticle, Quantity>> ingredients,
+                           final BeerDescription beerDescription,
+                           final BatchMethod method,
+                           final Quantity size,
+                           final WaterMeasurement measurement) {
+        return new ModifiableBatchInfoImpl(ingredients, beerDescription, method, size, measurement);
+    }
+
+    static ModifiableBatchInfo of(final Collection<Pair<IngredientArticle, Quantity>> ingredients,
+                           final BeerDescription beerDescription,
+                           final BatchMethod method,
+                           final Quantity size) {
+        return new ModifiableBatchInfoImpl(ingredients, beerDescription, method, size, null);
+    }
+
+}
