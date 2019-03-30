f55946a760952e6246b12fa2fb2218655cc388ed
Giacomo Pasini <g.pasini98@gmail.com>
2019-03-30 11:15:17 +0100

    make beerdescription mutable

--- src/main/java/nwoolcan/model/brewery/production/batch/misc/BeerDescription.java
+++ src/main/java/nwoolcan/model/brewery/production/batch/misc/BeerDescription.java
@@ -12,13 +12,25 @@ public interface BeerDescription {
      */
     String getName();
     /**
+     * Set the name of the beer.
+     */
+    void setName(String name);
+    /**
      * Return the style of the beer (es 'Doppelbock').
      * @return a {@link String}.
      */
     String getStyle();
     /**
+     * Set the style of the beer (es 'Doppelbock').
+     */
+    void setStyle(String style);
+    /**
      * Return the category of the style, if available (es 'Dark European Lager').
      * @return a {@link String}.
      */
     Optional<String> getStyleCategory();
+    /**
+     * Set the style category of the style, if available (es 'Dark European Lager').
+     */
+    void setStyleCategory(String name);
 }
