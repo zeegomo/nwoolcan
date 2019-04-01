b679f09d104db06298595a6d3b67b1de6f67899c
Carlovan <giulio.carlassare@gmail.com>
2019-04-01 11:28:04 +0200

    Created dummy interface

---
+++ src/main/java/nwoolcan/view/ViewController.java
@@ -0,0 +1,12 @@
+package nwoolcan.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ViewController {
+
+    @FXML
+    public void btnPrintClicked(ActionEvent event) {
+        System.out.println("printed");
+    }
+}
