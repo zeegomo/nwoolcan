54d177ade284181c261be22f2ba86f30adea2322
Samuele Ceroni <samuele.ceroni@gmail.com>
2019-04-02 17:56:56 +0200

    add dashboard viewmodel

---
+++ src/main/java/nwoolcan/view/viewModel/DashBoard.java
@@ -0,0 +1,21 @@
+package nwoolcan.view.viewModel;
+
+/**
+ * View-Model interface with methods callable to obtain general information about the warehouse.
+ * There will be information about:
+ * <ul>
+ *     <li>Number of {@link nwoolcan.model.brewery.production.batch.Batch} which are in progress;</li>
+ *     <li>{@link nwoolcan.model.brewery.warehouse.stock.Stock} which are going to expire;</li>
+ *     <li>{@link nwoolcan.model.brewery.warehouse.stock.Stock} which have a low {@link nwoolcan.model.utils.Quantity}</li>
+ * </ul>
+ */
+public interface DashBoard {
+
+    /**
+     * Returns the number of ongoing {@link nwoolcan.model.brewery.production.batch.Batch}.
+     * @return the number of ongoing {@link nwoolcan.model.brewery.production.batch.Batch}.
+     */
+    int getNumberOfOngoingBatches();
+
+
+}
