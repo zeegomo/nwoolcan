e1c88ea4a01b1dae4e59d3cf6db2914d9ed7f3e3
Samuele Ceroni <samuele.ceroni@gmail.com>
2019-04-07 16:07:11 +0200

    init interface

---
+++ src/main/java/nwoolcan/controller/brewery/BreweryController.java
@@ -0,0 +1,7 @@
+package nwoolcan.controller.brewery;
+
+/**
+ * General controller of the {@link nwoolcan.model.brewery.Brewery}.
+ */
+public interface BreweryController {
+}
---
+++ src/main/java/nwoolcan/controller/brewery/warehouse/WarehouseController.java
@@ -0,0 +1,14 @@
+package nwoolcan.controller.brewery.warehouse;
+
+import nwoolcan.model.brewery.warehouse.article.QueryArticle;
+import nwoolcan.model.brewery.warehouse.stock.QueryStock;
+import nwoolcan.viewmodel.brewery.warehouse.article.ArticleViewModel;
+import nwoolcan.viewmodel.brewery.warehouse.stock.StockViewModel;
+
+import java.util.List;
+
+/**
+ * Controller of the {@link nwoolcan.model.brewery.warehouse.Warehouse}.
+ */
+public interface WarehouseController {
+}
