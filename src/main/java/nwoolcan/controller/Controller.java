388099d5cbb02005099735b869dc1585987cb0ca
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-11 11:29:12 +0200

    fix old stuff with new stuff

--- src/main/java/nwoolcan/controller/Controller.java
+++ src/main/java/nwoolcan/controller/Controller.java
@@ -1,6 +1,11 @@
 package nwoolcan.controller;
 
 import nwoolcan.controller.brewery.BreweryController;
+import nwoolcan.model.brewery.production.batch.QueryBatch;
+import nwoolcan.viewmodel.brewery.production.ProductionViewModel;
+import nwoolcan.viewmodel.brewery.production.batch.MasterBatchViewModel;
+
+import java.util.List;
 
 /**
  * Controller.
@@ -11,4 +16,15 @@ public interface Controller {
      * @return the {@link BreweryController}.
      */
     BreweryController getBreweryController();
+    /**
+     * Returns the built view model for the view.
+     * @return the built view model for the view.
+     */
+    ProductionViewModel getProductionViewModel();
+    /**
+     * Returns the queried batches master view model from production.
+     * @param query filter for batches.
+     * @return the queried batches.
+     */
+    List<MasterBatchViewModel> getBatches(QueryBatch query);
 }
--- src/main/java/nwoolcan/controller/ControllerImpl.java
+++ src/main/java/nwoolcan/controller/ControllerImpl.java
@@ -3,12 +3,21 @@ package nwoolcan.controller;
 import nwoolcan.controller.brewery.BreweryController;
 import nwoolcan.controller.brewery.BreweryControllerImpl;
 import nwoolcan.model.brewery.Brewery;
+import nwoolcan.model.brewery.production.batch.QueryBatch;
+import nwoolcan.model.brewery.production.batch.QueryBatchBuilder;
+import nwoolcan.viewmodel.brewery.production.ProductionViewModel;
+import nwoolcan.viewmodel.brewery.production.batch.MasterBatchViewModel;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Controller basic implementation.
  */
 public final class ControllerImpl implements Controller {
 
+    private final Brewery brewery;
     private final BreweryController breweryController;
 
     /**
@@ -16,11 +25,24 @@ public final class ControllerImpl implements Controller {
      * @param model the model to inject,
      */
     public ControllerImpl(final Brewery model) {
+        this.brewery = model;
         this.breweryController = new BreweryControllerImpl(model);
     }
 
     @Override
     public BreweryController getBreweryController() {
         return this.breweryController;
+    }
+
+    @Override
+    public ProductionViewModel getProductionViewModel() {
+        return new ProductionViewModel(this.brewery.getBatches(new QueryBatchBuilder().build().getValue()));
+    }
+
+    @Override
+    public List<MasterBatchViewModel> getBatches(final QueryBatch query) {
+        return Collections.unmodifiableList(this.brewery.getBatches(query).stream()
+                                                        .map(MasterBatchViewModel::new)
+                                                        .collect(Collectors.toList()));
     }
 }
