f421ee5868dcce1a76a7fde94b02b16822c440f8
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-11 23:31:20 +0200

    first structure

--- src/main/java/nwoolcan/view/production/NewBatchModalController.java
+++ src/main/java/nwoolcan/view/production/NewBatchModalController.java
@@ -1,5 +1,8 @@
 package nwoolcan.view.production;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import nwoolcan.controller.Controller;
 import nwoolcan.view.AbstractViewController;
 import nwoolcan.view.InitializableController;
@@ -9,8 +12,14 @@ import nwoolcan.viewmodel.brewery.production.batch.NewBatchViewModel;
 /**
  * View controller for new batch modal.
  */
+@SuppressWarnings("NullAway")
 public class NewBatchModalController
     extends AbstractViewController implements InitializableController<NewBatchViewModel> {
+
+    @FXML
+    private TableView elementsTableView;
+    @FXML
+    private ComboBox elementsComboBox;
 
     /**
      * Creates itself and inject the controller and the view manager.
