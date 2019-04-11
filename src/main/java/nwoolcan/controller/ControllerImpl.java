77bed841c09b0b06f1a8b4151b4d8b802e3b07fc
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-11 19:08:35 +0200

    fix controller to use new funcs (not tested yet)

--- src/main/java/nwoolcan/controller/ControllerImpl.java
+++ src/main/java/nwoolcan/controller/ControllerImpl.java
@@ -7,8 +7,10 @@ import nwoolcan.model.brewery.production.batch.BatchBuilder;
 import nwoolcan.model.brewery.production.batch.QueryBatch;
 import nwoolcan.model.brewery.production.batch.QueryBatchBuilder;
 import nwoolcan.model.brewery.production.batch.misc.BeerDescriptionImpl;
-import nwoolcan.model.brewery.production.batch.misc.WaterMeasurementBuilder;
-import nwoolcan.model.brewery.production.batch.step.parameter.ParameterImpl;
+import nwoolcan.model.brewery.production.batch.misc.WaterMeasurement;
+import nwoolcan.model.brewery.production.batch.misc.WaterMeasurementFactory;
+import nwoolcan.model.brewery.production.batch.step.parameter.Parameter;
+import nwoolcan.model.brewery.production.batch.step.parameter.ParameterFactory;
 import nwoolcan.model.brewery.production.batch.step.parameter.ParameterTypeEnum;
 import nwoolcan.model.brewery.warehouse.article.ArticleType;
 import nwoolcan.model.brewery.warehouse.article.QueryArticleBuilder;
@@ -59,10 +61,10 @@ public final class ControllerImpl implements Controller {
 
     @Override
     public Result<Empty> createNewBatch(final CreateBatchDTO batchDTO) {
-        //creo il batch
+        //create batch builder
         final BatchBuilder bBuilder = this.brewery.getBatchBuilder();
 
-        //prendo gli ingredienti dalla brewery
+        //take all ingredients from warehouse that have ids equals to the ones in batchDTO
         this.brewery.getWarehouse().getArticles(
             new QueryArticleBuilder().setIncludeArticleType(ArticleType.INGREDIENT).build())
                     .stream()
@@ -75,28 +77,45 @@ public final class ControllerImpl implements Controller {
                                 .stream()
                                 .filter(p -> p.getLeft() == a.getId())
                                 .map(Pair::getRight).findAny().get()))
-                    .forEach(p -> bBuilder.addIngredient(p.getLeft(), p.getRight().intValue())); //TODO remove intValue
+                    .forEach(p -> bBuilder.addIngredient(p.getLeft(), p.getRight()));
 
-        //inserisco il water measurement
-        final WaterMeasurementBuilder wmBuilder = new WaterMeasurementBuilder();
+        Result<Empty> res = Result.ofEmpty();
 
-        batchDTO.getWaterMeasurement().forEach(t ->
-            wmBuilder.addRegistration(new ParameterImpl(//TODO use simple factory with result
-                ParameterTypeEnum.WATER_MEASUREMENT,
-                t.getMiddle(),
-                t.getRight()
-            ), t.getLeft())
-        );
+        //if there is at least one measurement, build it and set it
+        if (batchDTO.getWaterMeasurement().size() > 0) {
+            res = batchDTO.getWaterMeasurement()
+                          .stream()
+                          //map to correct representations
+                          .map(t -> Pair.of(
+                              t.getLeft(),
+                              ParameterFactory.create(
+                                  ParameterTypeEnum.WATER_MEASUREMENT,
+                                  t.getMiddle(),
+                                  t.getRight())))
+                          //propagate error of parameter construction out of pair
+                          .map(reg -> Result.of(reg)
+                                            .flatMap(r -> r.getRight()
+                                                           .map(rr -> Pair.of(r.getLeft(), rr))))
+                          //propagate errors in reduction from collection
+                          .reduce(Result.of(Collections.<Pair<WaterMeasurement.Element, Parameter>>emptyList()),
+                              (acc, r) -> acc.flatMap(col -> r.map(rr -> {
+                                  col.add(rr);
+                                  return col;
+                              })),
+                              (r1, r2) -> r1.flatMap(col1 -> r2.peek(col2 -> col2.addAll(col1))))
+                          .flatMap(WaterMeasurementFactory::create)
+                          .peek(bBuilder::setWaterMeasurement)
+                          .toEmpty();
+        }
 
-        wmBuilder.build().peek(bBuilder::setWaterMeasurement);
-
-        return bBuilder.build(
-            new BeerDescriptionImpl(batchDTO.getName(), batchDTO.getStyle(), batchDTO.getCategory().orElse(null)),
-            batchDTO.getMethod(),
-            batchDTO.getIntialSize(),
-            batchDTO.getInitialStep())
-                       .peek(this.brewery::addBatch)
-                       .toEmpty();
+        //finally build the batch and add it to the brewery
+        return res.flatMap(e -> bBuilder.build(
+                        new BeerDescriptionImpl(batchDTO.getName(), batchDTO.getStyle(), batchDTO.getCategory().orElse(null)),
+                        batchDTO.getMethod(),
+                        batchDTO.getIntialSize(),
+                        batchDTO.getInitialStep()))
+                  .peek(this.brewery::addBatch)
+                  .toEmpty();
     }
 
 
