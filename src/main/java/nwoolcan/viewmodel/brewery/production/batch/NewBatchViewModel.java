a97929d47488774b9658d489ee29db13b9a4b006
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-11 22:59:57 +0200

    implementation of view model

--- src/main/java/nwoolcan/viewmodel/brewery/production/batch/NewBatchViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/production/batch/NewBatchViewModel.java
@@ -1,7 +1,43 @@
 package nwoolcan.viewmodel.brewery.production.batch;
 
+import nwoolcan.model.brewery.production.batch.misc.WaterMeasurement;
+import nwoolcan.viewmodel.brewery.warehouse.article.IngredientArticleViewModel;
+
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * View model representing the data needed to show the new batch modal.
  */
 public class NewBatchViewModel {
+
+    private final Collection<IngredientArticleViewModel> ingredients;
+    private final Collection<WaterMeasurement.Element> waterMeasurementElements;
+
+    /**
+     * Basic constructor.
+     * @param ingredients all possible ingredients that can be chosen.
+     * @param waterMeasurementElements all possible elements that can be chosen.
+     */
+    public NewBatchViewModel(final Collection<IngredientArticleViewModel> ingredients,
+                             final Collection<WaterMeasurement.Element> waterMeasurementElements) {
+        this.ingredients = Collections.unmodifiableCollection(ingredients);
+        this.waterMeasurementElements = Collections.unmodifiableCollection(waterMeasurementElements);
+    }
+
+    /**
+     * Returns a collection of all possible ingredients to use for batch creation.
+     * @return all possible ingredients that can be chosen.
+     */
+    public Collection<IngredientArticleViewModel> getIngredients() {
+        return Collections.unmodifiableCollection(ingredients);
+    }
+
+    /**
+     * Returns a collection of all possible elements to use for batch creation.
+     * @return all possible elements that can be chosen.
+     */
+    public Collection<WaterMeasurement.Element> getWaterMeasurementElements() {
+        return Collections.unmodifiableCollection(waterMeasurementElements);
+    }
 }
