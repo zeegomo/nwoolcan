5eec30cded2a50c6689b5d92e8cfd1d77e7890a5
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-04-11 17:42:50 +0200

    refactor of breweryViewModel

--- src/main/java/nwoolcan/viewmodel/brewery/BreweryViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/BreweryViewModel.java
@@ -15,47 +15,51 @@ import java.util.stream.Collectors;
  */
 public final class BreweryViewModel {
 
-    private final Brewery brewery;
+    private final Optional<String> breweryName;
+    private final Optional<String> ownerName;
+    private final WarehouseViewModel warehouse;
+    private final Collection<MasterBatchViewModel> batches;
 
     /**
      * Constructor with decorator pattern.
      * @param brewery to be converted to {@link BreweryViewModel}.
      */
     public BreweryViewModel(final Brewery brewery) {
-        this.brewery = brewery;
+        this.breweryName = brewery.getBreweryName();
+        this.ownerName = brewery.getOwnerName();
+        this.warehouse = new WarehouseViewModel(brewery.getWarehouse());
+        final QueryBatch allBatchesQuery = new QueryBatchBuilder().build().getValue();
+        this.batches = brewery.getBatches(allBatchesQuery)
+                              .stream()
+                              .map(MasterBatchViewModel::new)
+                              .collect(Collectors.toList());
     }
     /**
      * Getter of the name of the {@link Brewery}.
      * @return an {@link Optional} of {@link String} with the name of the {@link Brewery}.
      */
     public Optional<String> getBreweryName() {
-        return brewery.getBreweryName();
+        return breweryName;
     }
     /**
      * Getter of the name of the owner of the {@link Brewery}.
      * @return an {@link Optional} of {@link String} with the name of the owner of the {@link Brewery}.
      */
     public Optional<String> getOwnerName() {
-        return brewery.getOwnerName();
+        return ownerName;
     }
     /**
      * Getter of the {@link WarehouseViewModel} of the {@link Brewery}.
      * @return the only {@link WarehouseViewModel} of the {@link Brewery}.
      */
     public WarehouseViewModel getWarehouse() {
-        return new WarehouseViewModel(brewery.getWarehouse());
+        return warehouse;
     }
     /**
      * Return all the {@link MasterBatchViewModel} of the {@link Brewery}.
      * @return a {@link Collection} of {@link MasterBatchViewModel} of the {@link Brewery}.
      */
     public Collection<MasterBatchViewModel> getAllBatches() {
-        final QueryBatch allBatchesQuery = new QueryBatchBuilder().build().getValue();
-        return brewery.getBatches(allBatchesQuery)
-                      .stream()
-                      .map(MasterBatchViewModel::new)
-                      .collect(Collectors.toList());
+        return batches;
     }
-
-
 }
