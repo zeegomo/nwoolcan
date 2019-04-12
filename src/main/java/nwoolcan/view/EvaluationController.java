47757e60cd23eae17306229369b0ca5f075c6c3e
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-12 18:50:22 +0200

    add controller for evaluation view

---
+++ src/main/java/nwoolcan/view/EvaluationController.java
@@ -0,0 +1,32 @@
+package nwoolcan.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import nwoolcan.viewmodel.brewery.production.batch.review.EvaluationViewModel;
+
+@SuppressWarnings("NullAway")
+public final class EvaluationController implements InitializableController<EvaluationViewModel> {
+    @FXML
+    private TitledPane title;
+    @FXML
+    private Label score;
+    @FXML
+    private Label notes;
+
+    @Override
+    public void initData(final EvaluationViewModel data) {
+        title.setText(data.getType());
+        title.setCollapsible(false);
+        score.setText(formatScore(data));
+        notes.setText(formatNotes(data));
+    }
+
+    private String formatScore(final EvaluationViewModel data) {
+        return data.getScore() + "/" + data.getMaxScore();
+    }
+
+    private String formatNotes(final EvaluationViewModel data) {
+        return data.getNotes().orElse("");
+    }
+}
