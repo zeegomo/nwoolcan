2a0627e24273244de9d9312c6324f4446e6ca246
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-04-12 14:58:16 +0200

    fix javadoc and errors

---
+++ src/main/java/nwoolcan/view/brewery/warehouse/article/ArticlesViewController.java
@@ -0,0 +1,13 @@
+package nwoolcan.view.brewery.warehouse.article;
+
+import javafx.scene.layout.AnchorPane;
+
+import javax.annotation.Nullable;
+
+/**
+ * Articles viewController.
+ */
+public class ArticlesViewController {
+
+    @Nullable private AnchorPane masterArticleSubView;
+}
--- src/main/java/nwoolcan/view/brewery/warehouse/article/MasterArticles.java
+++
@@ -1,7 +0,0 @@
-package nwoolcan.view.brewery.warehouse.article;
-
-import javafx.scene.layout.AnchorPane;
-
-public class MasterArticles {
-    public AnchorPane masterArticleSubView;
-}
