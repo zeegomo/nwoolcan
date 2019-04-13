a8899efa92ed5535841c9222fcb780c13159e0ef
Carlovan <giulio.carlassare@gmail.com>
2019-04-13 12:33:13 +0200

    (De)serialization of generic classes

--- src/main/java/nwoolcan/model/database/InterfaceAdapterFactory.java
+++ src/main/java/nwoolcan/model/database/InterfaceAdapterFactory.java
@@ -1,13 +1,20 @@
 package nwoolcan.model.database;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
 
-public class InterfaceAdapterFactory implements TypeAdapterFactory {
+/**
+ * Factory to handle polymorphic type.
+ */
+public final class InterfaceAdapterFactory implements TypeAdapterFactory { // TODO better naming
 
     @Override
     public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type) {
@@ -22,6 +29,12 @@ public class InterfaceAdapterFactory implements TypeAdapterFactory {
                 }
             }
 
+            /**
+             * Wraps the actual object inside an object with a single property: the runtime class name.
+             * @param out Output writer.
+             * @param value The value to serialize.
+             * @throws IOException When something goes wrong while writing to out.
+             */
             @Override
             public void write(final JsonWriter out, final T value) throws IOException {
                 if (value == null) {
@@ -35,11 +48,22 @@ public class InterfaceAdapterFactory implements TypeAdapterFactory {
                 out.endObject();
             }
 
+            /**
+             * De-serializes a Json using the runtime type specified by {@link #read(JsonReader)}. If the actual compile-time type is parametrized, applies the same type parameters to the runtime type.
+             * @param in Input reader.
+             * @return The deserialized object.
+             * @throws IOException When something goes wrong reading to in.
+             */
             @Override
             public T read(final JsonReader in) throws IOException {
                 in.beginObject();
                 final String className = in.nextName();
-                final TypeToken<?> objType = TypeToken.get(this.getClassFromName(className));
+                final TypeToken<?> objType;
+                if (type.getType() instanceof ParameterizedType) {
+                    objType = TypeToken.getParameterized(this.getClassFromName(className), ((ParameterizedType) type.getType()).getActualTypeArguments());
+                } else {
+                    objType = TypeToken.get(this.getClassFromName(className));
+                }
                 final T obj = (T) gson.getDelegateAdapter(thisFactory, objType).read(in);
                 in.endObject();
                 return obj;
