17fbaa753cb0dbcfaecada2de797b5abef24af0d
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-14 17:36:40 +0200

    add javadoc and remove unused imports

--- src/main/java/nwoolcan/controller/BatchController.java
+++ src/main/java/nwoolcan/controller/BatchController.java
@@ -2,10 +2,8 @@ package nwoolcan.controller;
 
 import nwoolcan.model.brewery.Brewery;
 import nwoolcan.model.brewery.batch.Batch;
-import nwoolcan.model.brewery.batch.BatchBuilder;
 import nwoolcan.model.brewery.batch.QueryBatch;
 import nwoolcan.model.brewery.batch.QueryBatchBuilder;
-import nwoolcan.model.brewery.batch.review.BatchEvaluation;
 import nwoolcan.model.brewery.batch.review.BatchEvaluationBuilder;
 import nwoolcan.model.brewery.batch.review.BatchEvaluationType;
 import nwoolcan.model.brewery.batch.review.Evaluation;
@@ -14,14 +12,10 @@ import nwoolcan.utils.Empty;
 import nwoolcan.utils.Result;
 import nwoolcan.viewmodel.brewery.production.batch.review.BatchEvaluationDTO;
 import nwoolcan.viewmodel.brewery.production.batch.review.BatchEvaluationDetailViewModel;
-import nwoolcan.viewmodel.brewery.production.batch.review.BatchEvaluationViewModel;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class BatchController {
 
@@ -36,7 +30,6 @@ public class BatchController {
         this.brewery = model;
     }
 
-    @Override
     public Result<Batch> getBatchById(final int batchId) {
         final QueryBatch querySingleBatch = new QueryBatchBuilder().setMinId(batchId)
                                                                    .setMaxId(batchId)
@@ -46,19 +39,34 @@ public class BatchController {
                      .map(batches -> batches.iterator().next());
     }
 
+    /**
+     * Return the evaluation for specified batch if available.
+     * @param batchID the batch.
+     * @return the evaluation for specified batch if available.
+     */
     public Result<Optional<BatchEvaluationDetailViewModel>> getBatchEvaluation(final int batchID) {
         return getBatchById(batchID).map(batch -> batch.getEvaluation().map(BatchEvaluationDetailViewModel::new));
     }
 
+    /**
+     * Return all available {@link BatchEvaluationType};
+     * @return all available {@link BatchEvaluationType};
+     */
     public Result<Set<BatchEvaluationType>> getAvailableBatchEvaluationTypes() {
         return BatchEvaluationBuilder.getAvailableBatchEvaluationTypes();
     }
 
+    /**
+     * Register an evaluation for the specified batch.
+     * @param batchID the id of the batch.
+     * @param newBatch the new evaluation.
+     * @return a Result describing the outcome of the operation.
+     */
     public Result<Empty> addBatchEvaluation(final int batchID, final BatchEvaluationDTO newBatch) {
         return newBatch.getCategories()
                        .stream()
                        .map(cat -> EvaluationFactory.create(cat.getLeft(), cat.getMiddle(), cat.getRight().orElse(null)))
-                       .<Result<Set<Evaluation>>>reduce(
+                       .reduce(
                            Result.of(new HashSet<>()),
                            (res, cat) -> res.require(cat::isPresent, cat.getError())
                                  .peek(list -> list.add(cat.getValue())),
