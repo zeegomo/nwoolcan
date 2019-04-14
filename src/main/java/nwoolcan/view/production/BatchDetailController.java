9736457094593ec212ce937f2336761949123e12
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-14 15:03:05 +0200

    add go back button and some panes

--- src/main/java/nwoolcan/view/production/BatchDetailController.java
+++ src/main/java/nwoolcan/view/production/BatchDetailController.java
@@ -1,6 +1,8 @@
 package nwoolcan.view.production;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import nwoolcan.controller.Controller;
 import nwoolcan.view.InitializableController;
@@ -9,15 +11,19 @@ import nwoolcan.view.ViewManager;
 import nwoolcan.view.subview.SubView;
 import nwoolcan.viewmodel.brewery.production.batch.DetailBatchViewModel;
 
+/**
+ * View controller for the batch detail view.
+ */
 @SuppressWarnings("NullAway")
-public class BatchDetailController
+public final class BatchDetailController
     extends SubViewController
     implements InitializableController<DetailBatchViewModel> {
 
     @FXML
-    private SubView batchDetailSubView;
+    private Button goBackButton;
+
     @FXML
-    private Label batchIdLabel;
+    private SubView batchDetailSubView;
 
     /**
      * Creates itself and gets injected.
@@ -31,11 +37,19 @@ public class BatchDetailController
 
     @Override
     public void initData(final DetailBatchViewModel data) {
-        this.batchIdLabel.setText(String.valueOf(data.getId()));
+
     }
 
     @Override
     protected SubView getSubView() {
         return this.batchDetailSubView;
+    }
+
+    /**
+     * Goes back to the production view.
+     * @param event the occurred event.
+     */
+    public void goBackButtonClicked(final ActionEvent event) {
+        this.previousView();    //TODO this does not reload the production view
     }
 }
