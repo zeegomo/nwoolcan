2c1026f9f7870a10f2a9a228e86603b97321c2d0
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-14 15:27:49 +0200

    show evaluation type name in title

--- src/main/java/nwoolcan/view/review/EvaluationTypeController.java
+++ src/main/java/nwoolcan/view/review/EvaluationTypeController.java
@@ -4,7 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import nwoolcan.model.brewery.production.batch.review.EvaluationType;
+import javafx.scene.control.TitledPane;
+import nwoolcan.model.brewery.batch.review.EvaluationType;
 import nwoolcan.view.InitializableController;
 
 @SuppressWarnings("NullAway")
@@ -15,9 +16,12 @@ public class EvaluationTypeController implements InitializableController<Evaluat
     private Label maxScore;
     @FXML
     private TextArea notes;
+    @FXML
+    private TitledPane title;
 
     @Override
     public void initData(final EvaluationType data) {
         this.maxScore.setText(String.valueOf(data.getMaxScore()));
+        this.title.setText(data.getName());
     }
 }
