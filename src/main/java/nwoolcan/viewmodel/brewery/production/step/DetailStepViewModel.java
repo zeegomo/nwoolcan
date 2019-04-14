8c8631ed422823c18d2e3124584c26a37db526de
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-14 12:57:40 +0200

    add detail step view model

--- src/main/java/nwoolcan/viewmodel/brewery/production/step/DetailStepViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/production/step/DetailStepViewModel.java
@@ -1,16 +1,17 @@
 package nwoolcan.viewmodel.brewery.production.step;
 
 import nwoolcan.model.brewery.batch.step.Step;
-import nwoolcan.model.brewery.batch.step.StepType;
-import nwoolcan.model.brewery.batch.step.parameter.Parameter;
-import nwoolcan.model.utils.Quantity;
+import nwoolcan.model.brewery.batch.step.parameter.QueryParameterBuilder;
 import nwoolcan.viewmodel.brewery.utils.QuantityViewModel;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class DetailStepViewModel {
+public final class DetailStepViewModel {
 
     private final String typeName;
     private final String notes;
@@ -21,9 +22,52 @@ public class DetailStepViewModel {
     @Nullable
     private final QuantityViewModel endSize;
 
-    private final List<ParameterViewModel>
+    private final boolean finalized;
+
+    private final List<ParameterViewModel> registeredParameters;
 
     public DetailStepViewModel(final Step step) {
+        this.typeName = step.getStepInfo().getType().getName();
+        this.notes = step.getStepInfo().getNote().orElse("");
+        this.startDate = new Date(step.getStepInfo().getStartDate().getTime());
+        this.endDate = step.getStepInfo().getEndDate().map(d -> new Date(d.getTime())).orElse(null);
+        this.endSize = step.getStepInfo().getEndStepSize().map(QuantityViewModel::new).orElse(null);
+        this.finalized = step.isFinalized();
+        this.registeredParameters = step.getParameters(new QueryParameterBuilder().sortByDate(true)
+                                                                                  .sortDescending(true)
+                                                                                  .build().getValue())
+                                        .stream()
+                                        .map(ParameterViewModel::new)
+                                        .collect(Collectors.toList());
+    }
 
+    public String getTypeName() {
+        return this.typeName;
+    }
+
+    public String getNotes() {
+        return this.notes;
+    }
+
+    public Date getStartDate() {
+        return new Date(this.startDate.getTime());
+    }
+
+    @Nullable
+    public Date getEndDate() {
+        return Optional.ofNullable(this.endDate).map(d -> new Date(this.endDate.getTime())).orElse(null);
+    }
+
+    @Nullable
+    public QuantityViewModel getEndSize() {
+        return this.endSize;
+    }
+
+    public boolean isFinalized() {
+        return finalized;
+    }
+
+    public List<ParameterViewModel> getRegisteredParameters() {
+        return Collections.unmodifiableList(this.registeredParameters);
     }
 }
