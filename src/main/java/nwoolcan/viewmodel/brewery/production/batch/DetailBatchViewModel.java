d45c2c221c299d2e8588e682e4fd79998ae2486d
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-14 14:18:21 +0200

    add javadoc

--- src/main/java/nwoolcan/viewmodel/brewery/production/batch/DetailBatchViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/production/batch/DetailBatchViewModel.java
@@ -5,15 +5,14 @@ import nwoolcan.model.brewery.batch.Batch;
 import nwoolcan.viewmodel.brewery.production.batch.review.EvaluationViewModel;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Class representing the detail view model of a batch.
+ * View model representing a detailed step.
  */
-public class DetailBatchViewModel {
+public final class DetailBatchViewModel {
 
     private final BatchInfoViewModel batchInfo;
     private final List<MasterStepViewModel> steps;
@@ -21,20 +20,36 @@ public class DetailBatchViewModel {
     @Nullable
     private final EvaluationViewModel review;
 
+    /**
+     * Basic constructor with decorator-like pattern.
+     * @param batch the batch to get the data from.
+     */
     public DetailBatchViewModel(final Batch batch) {
         this.batchInfo = new BatchInfoViewModel(); //TODO change to correct implementation when done
         this.steps = batch.getSteps().stream().map(MasterStepViewModel::new).collect(Collectors.toList());
         this.review = batch.getEvaluation().map(EvaluationViewModel::new).orElse(null);
     }
 
+    /**
+     * Returns the batch info view model of the batch.
+     * @return the batch info view model of the batch.
+     */
     public BatchInfoViewModel getBatchInfo() {
         return this.batchInfo;
     }
 
+    /**
+     * Returns a list of {@link MasterStepViewModel} each one representing a step in production of the batch.
+     * @return a list of {@link MasterStepViewModel} each one representing a step in production of the batch.
+     */
     public List<MasterStepViewModel> getSteps() {
         return Collections.unmodifiableList(this.steps);
     }
 
+    /**
+     * Returns the evaluation view model of the batch representing its review (null if none).
+     * @return the evaluation view model of the batch representing its review (null if none).
+     */
     @Nullable
     public EvaluationViewModel getReview() {
         return this.review;
--- src/main/java/nwoolcan/viewmodel/brewery/production/batch/MasterStepViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/production/batch/MasterStepViewModel.java
@@ -8,7 +8,10 @@ import javax.annotation.Nullable;
 import java.util.Date;
 import java.util.Optional;
 
-public class MasterStepViewModel {
+/**
+ * View model representing a step in a master table view.
+ */
+public final class MasterStepViewModel {
 
     private final StepType type;
     private final Date startDate;
@@ -20,6 +23,10 @@ public class MasterStepViewModel {
 
     private final boolean finalized;
 
+    /**
+     * Basic constructor with decorator-like pattern.
+     * @param step the step to get data from.
+     */
     public MasterStepViewModel(final Step step) {
         this.type = step.getStepInfo().getType();
         this.startDate = new Date(step.getStepInfo().getStartDate().getTime());
@@ -28,24 +35,44 @@ public class MasterStepViewModel {
         this.finalized = step.isFinalized();
     }
 
+    /**
+     * Returns the step type.
+     * @return the step type.
+     */
     public StepType getType() {
         return this.type;
     }
 
+    /**
+     * Returns the start date of the step.
+     * @return the start date of the step.
+     */
     public Date getStartDate() {
         return new Date(this.startDate.getTime());
     }
 
+    /**
+     * Returns the end date of the step (null if none).
+     * @return the end date of the step (null if none).
+     */
     @Nullable
     public Date getEndDate() {
         return Optional.ofNullable(this.endDate).map(d -> new Date(d.getTime())).orElse(null);
     }
 
+    /**
+     * Returns the {@link QuantityViewModel} representing the end size of the step.
+     * @return the {@link QuantityViewModel} representing the end size of the step.
+     */
     @Nullable
     public QuantityViewModel getEndSize() {
         return this.endSize;
     }
 
+    /**
+     * Returns true if the step is finalized, false otherwise.
+     * @return true if the step is finalized.
+     */
     public boolean isFinalized() {
         return this.finalized;
     }
