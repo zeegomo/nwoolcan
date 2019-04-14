986c33f090f81854f1adb60c151bfcccc842a5e9
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-14 14:32:45 +0200

    batch controller first implementation

---
+++ src/main/java/nwoolcan/controller/batch/BatchControllerImpl.java
@@ -0,0 +1,40 @@
+package nwoolcan.controller.batch;
+
+import nwoolcan.model.brewery.Brewery;
+import nwoolcan.model.brewery.batch.Batch;
+import nwoolcan.model.brewery.batch.QueryBatchBuilder;
+import nwoolcan.utils.Result;
+import nwoolcan.viewmodel.brewery.production.batch.DetailBatchViewModel;
+
+import java.util.Optional;
+
+/**
+ * Basic implementation of {@link BatchController} interface.
+ */
+public final class BatchControllerImpl implements BatchController {
+
+    private static final String BATCH_NOT_FOUND_MESSAGE = "Cannot find the batch with id: ";
+    private final Brewery model;
+
+    /**
+     * Basic constructor with reference to the {@link Brewery} model.
+     * @param model the model to use as reference.
+     */
+    public BatchControllerImpl(final Brewery model) {
+        this.model = model;
+    }
+
+    @Override
+    public Result<DetailBatchViewModel> getDetailBatchViewModelById(final int batchId) {
+        final Optional<Batch> batch = this.model.getBatches(new QueryBatchBuilder().setMinId(batchId)
+                                                                                   .setMaxId(batchId)
+                                                                                   .build()
+                                                                                   .getValue())
+                                                .stream()
+                                                .findAny();
+        return Result.of(batch)
+                     .require(Optional::isPresent, new IllegalArgumentException(BATCH_NOT_FOUND_MESSAGE + batchId))
+                     .map(Optional::get)
+                     .map(DetailBatchViewModel::new);
+    }
+}
