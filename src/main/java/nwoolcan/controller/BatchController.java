50bb5a56f9b16273e8b4af523cc45802d9f04037
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-14 17:31:24 +0200

    add addNewEvaluationMethod

--- src/main/java/nwoolcan/controller/BatchController.java
+++ src/main/java/nwoolcan/controller/BatchController.java
@@ -29,6 +29,7 @@ public class BatchController {
 
     /**
      * Basic constructor with reference to the {@link Brewery} model.
+     *
      * @param model the model to use as reference.
      */
     public BatchController(final Brewery model) {
@@ -48,22 +49,29 @@ public class BatchController {
     public Result<Optional<BatchEvaluationDetailViewModel>> getBatchEvaluation(final int batchID) {
         return getBatchById(batchID).map(batch -> batch.getEvaluation().map(BatchEvaluationDetailViewModel::new));
     }
+
     public Result<Set<BatchEvaluationType>> getAvailableBatchEvaluationTypes() {
         return BatchEvaluationBuilder.getAvailableBatchEvaluationTypes();
     }
-    public Result<Empty> addBatchEvaluation(final int batchID, final BatchEvaluationDTO newBatch) {
-        Result<Set<Evaluation>> categories = newBatch.getCategories()
-                                               .stream()
-                                               .map(cat -> EvaluationFactory.create(cat.getLeft(), cat.getMiddle(), cat.getRight().orElse(null)))
-                                               .reduce(
-                                                   Result.of(new HashSet<>()),
-                                                   (res, cat) -> res.require(cat::isPresent, cat.getError())
-                                                                    .peek(list -> list.add(cat.getValue())),
-                                                   (res1, res2) -> res1.require(res2::isPresent, res2.getError())
-                                                                       .peek(list -> list.addAll(res2.orElse(HashSet::new))));
 
-        BatchEvaluationBuilder builder = new BatchEvaluationBuilder();
-        newBatch.getNotes().ifPresent(builder::addNotes);
+    public Result<Empty> addBatchEvaluation(final int batchID, final BatchEvaluationDTO newBatch) {
+        return newBatch.getCategories()
+                       .stream()
+                       .map(cat -> EvaluationFactory.create(cat.getLeft(), cat.getMiddle(), cat.getRight().orElse(null)))
+                       .<Result<Set<Evaluation>>>reduce(
+                           Result.of(new HashSet<>()),
+                           (res, cat) -> res.require(cat::isPresent, cat.getError())
+                                 .peek(list -> list.add(cat.getValue())),
+                           (res1, res2) -> res1.require(res2::isPresent, res2.getError())
+                                    .peek(list -> list.addAll(res2.orElse(HashSet::new))))
+                       .flatMap(cat -> {
+                           BatchEvaluationBuilder builder = new BatchEvaluationBuilder();
+                           newBatch.getNotes().ifPresent(builder::addNotes);
+                           newBatch.getReviewer().ifPresent(builder::addReviewer);
+                           return new BatchEvaluationBuilder().build(newBatch.getBatchEvaluationType(), cat)
+                                                              .map(eval -> getBatchById(batchID).map(b -> b.setEvaluation(eval)));
+                       })
+                       .toEmpty();
     }
 
 }
