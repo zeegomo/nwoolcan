72f3a615e0d0af529334e48d8be03e6e4689d41b
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-14 18:05:53 +0200

    add GoNextStepViewModel getter

--- src/main/java/nwoolcan/controller/batch/BatchController.java
+++ src/main/java/nwoolcan/controller/batch/BatchController.java
@@ -2,6 +2,7 @@ package nwoolcan.controller.batch;
 
 import nwoolcan.utils.Result;
 import nwoolcan.viewmodel.brewery.production.batch.DetailBatchViewModel;
+import nwoolcan.viewmodel.brewery.production.batch.GoNextStepViewModel;
 
 /**
  * Interface representing a batch controller.
@@ -14,4 +15,12 @@ public interface BatchController {
      * @return a {@link Result} bearing the representation of the batch or with an error if no batch with this id is found.
      */
     Result<DetailBatchViewModel> getDetailBatchViewModelById(int batchId);
+
+    /**
+     * Returns a {@link GoNextStepViewModel} with the informations to choose the next step to go in the batch with
+     * specified id.
+     * @param batchId the batch id.
+     * @return a {@link Result} bearing the informations to choose choose the next step to go.
+     */
+    Result<GoNextStepViewModel> getGoNextStepViewModel(int batchId);
 }
