37978b9c9fdccf298fea40e2a20ba6749d6985c8
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-04-15 21:51:09 +0200

    table creation in fxml

--- src/main/java/nwoolcan/view/warehouse/stock/StockDetailController.java
+++ src/main/java/nwoolcan/view/warehouse/stock/StockDetailController.java
@@ -6,23 +6,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+
 import nwoolcan.controller.Controller;
 import nwoolcan.utils.Result;
 import nwoolcan.view.InitializableController;
 import nwoolcan.view.SubViewController;
 import nwoolcan.view.ViewManager;
 import nwoolcan.view.ViewType;
-import nwoolcan.view.mastertable.ColumnDescriptor;
 import nwoolcan.view.subview.SubView;
 import nwoolcan.viewmodel.brewery.warehouse.article.AbstractArticleViewModel;
 import nwoolcan.viewmodel.brewery.warehouse.stock.DetailStockViewModel;
 import nwoolcan.viewmodel.brewery.warehouse.stock.RecordViewModel;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Controller for the Stock detail view.
@@ -93,17 +90,6 @@ public final class StockDetailController extends SubViewController implements In
     }
 
     private void setTable(final List<RecordViewModel> articles) {
-       Stream.of(
-                    new ColumnDescriptor("Date", "date"),
-                    new ColumnDescriptor("Action", "action"),
-                    new ColumnDescriptor("Quantity", "quantity")
-                )
-             .map(cd -> {
-                            final TableColumn<RecordViewModel, String> col = new TableColumn<>(cd.getColumnName());
-                            col.setCellValueFactory(new PropertyValueFactory<>(cd.getFieldName()));
-                            return col;
-                        })
-             .forEach(tc -> recordTable.getColumns().add(tc));
        recordTable.setItems(FXCollections.observableArrayList(articles));
     }
 
