ff9a42ffaf7c9b1df243d0fe8954ca69697598c5
GiackAloZ <giacomo.aloisi1998@gmail.com>
2019-04-15 22:02:13 +0200

    add unit of measure info`

--- src/main/java/nwoolcan/viewmodel/brewery/production/batch/StockBatchViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/production/batch/StockBatchViewModel.java
@@ -1,5 +1,6 @@
 package nwoolcan.viewmodel.brewery.production.batch;
 
+import nwoolcan.model.utils.UnitOfMeasure;
 import nwoolcan.viewmodel.brewery.warehouse.article.BeerArticleViewModel;
 
 import java.util.Collections;
@@ -11,15 +12,18 @@ import java.util.List;
 public final class StockBatchViewModel {
 
     private final int batchId;
+    private final UnitOfMeasure unitOfMeasure;
     private final List<BeerArticleViewModel> possibleArticles;
 
     /**
      * Basic constructor.
      * @param batchId the batch id.
+     * @param unitOfMeasure the unit of measure of the new possible article created before stocking.
      * @param possibleArticles all possible beer article that can be used to stock the batch.
      */
-    public StockBatchViewModel(final int batchId, final List<BeerArticleViewModel> possibleArticles) {
+    public StockBatchViewModel(final int batchId, final UnitOfMeasure unitOfMeasure, final List<BeerArticleViewModel> possibleArticles) {
         this.batchId = batchId;
+        this.unitOfMeasure = unitOfMeasure;
         this.possibleArticles = Collections.unmodifiableList(possibleArticles);
     }
 
@@ -29,6 +33,14 @@ public final class StockBatchViewModel {
      */
     public int getBatchId() {
         return this.batchId;
+    }
+
+    /**
+     * Returns the unit of measure of the new possible beer article to create.
+     * @return the unit of measure of the new possible beer article to create.
+     */
+    public UnitOfMeasure getUnitOfMeasure() {
+        return this.unitOfMeasure;
     }
 
     /**
