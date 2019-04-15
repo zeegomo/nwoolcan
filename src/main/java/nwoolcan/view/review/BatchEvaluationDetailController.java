3b02a9c7020c6aa3f5e402dbf05b8516319f0b73
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-15 13:31:17 +0200

    wrap vbox height

--- src/main/java/nwoolcan/view/review/BatchEvaluationDetailController.java
+++ src/main/java/nwoolcan/view/review/BatchEvaluationDetailController.java
@@ -5,8 +5,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -33,12 +35,17 @@ public final class BatchEvaluationDetailController extends SubViewController
     implements InitializableController<BatchEvaluationDetailViewModel> {
 
     private static final String LOAD_FAILED = "Load failed";
+    private static final Double wrap = 1.50;
     @FXML
     private SubViewContainer container;
     @FXML
     private SubView batchEvaluationDetailSubView;
     @FXML
     private VBox categories;
+    @FXML
+    private ScrollPane categoriesScrollPane;
+    @FXML
+    private Text notes;
 
     /**
      * Creates itself and gets injected.
@@ -57,6 +64,9 @@ public final class BatchEvaluationDetailController extends SubViewController
             .peekError(err -> Logger.getGlobal().severe(err::toString));
         data.getCategories()
             .forEach(cat -> categories.getChildren().add(evaluationNode(cat)));
+        this.notes.setText(data.getInfo().getNotes().orElse(""));
+        this.notes.wrappingWidthProperty().bind(this.batchEvaluationDetailSubView.widthProperty().divide(2));
+        this.categoriesScrollPane.prefHeightProperty().bind(this.batchEvaluationDetailSubView.heightProperty().divide(wrap));
     }
 
     private Node evaluationNode(final EvaluationViewModel data) {
