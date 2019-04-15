5fca7fff170f7534b09c2d392394bd405f63aaf8
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-04-15 17:13:59 +0200

    first fxml implementation

--- src/main/java/nwoolcan/view/warehouse/stock/NewStockModalViewController.java
+++ src/main/java/nwoolcan/view/warehouse/stock/NewStockModalViewController.java
@@ -1,4 +1,24 @@
 package nwoolcan.view.warehouse.stock;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class NewStockModalViewController {
+    public ComboBox comboBoxArticle;
+    public CheckBox checkBoxDate;
+    public ComboBox comboMinuteSelection;
+    public ComboBox comboHourSelection;
+    public VBox boxDateTimePicker;
+    public Button createStockButton;
+
+    public void specifyDateClick(ActionEvent actionEvent) {
+    }
+
+    public void createStockClick(ActionEvent actionEvent) {
+    }
 }
