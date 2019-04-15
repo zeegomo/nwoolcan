b429a520bcaf72e601a6b231d48fa83748fdd9a3
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-15 13:58:50 +0200

    add methods from old controller

--- src/main/java/nwoolcan/controller/batch/BatchControllerImpl.java
+++ src/main/java/nwoolcan/controller/batch/BatchControllerImpl.java
@@ -3,13 +3,21 @@ package nwoolcan.controller.batch;
 import nwoolcan.controller.ControllerUtils;
 import nwoolcan.model.brewery.Brewery;
 import nwoolcan.model.brewery.batch.Batch;
+import nwoolcan.model.brewery.batch.review.BatchEvaluationBuilder;
+import nwoolcan.model.brewery.batch.review.Evaluation;
+import nwoolcan.model.brewery.batch.review.EvaluationFactory;
 import nwoolcan.utils.Empty;
 import nwoolcan.utils.Result;
 import nwoolcan.viewmodel.brewery.production.batch.DetailBatchViewModel;
 import nwoolcan.viewmodel.brewery.production.batch.GoNextStepDTO;
 import nwoolcan.viewmodel.brewery.production.batch.GoNextStepViewModel;
+import nwoolcan.viewmodel.brewery.production.batch.review.BatchEvaluationDTO;
+import nwoolcan.viewmodel.brewery.production.batch.review.BatchEvaluationDetailViewModel;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Basic implementation of {@link BatchController} interface.
@@ -57,5 +65,30 @@ public final class BatchControllerImpl implements BatchController {
         }
 
         return res.flatMap(b -> b.moveToNextStep(dto.getNextStepType()));
+    }
+
+    @Override
+    public Result<Empty> addBatchEvaluation(final int batchID, final BatchEvaluationDTO newBatch) {
+        return newBatch.getCategories()
+                       .stream()
+                       .map(cat -> EvaluationFactory.create(cat.getLeft(), cat.getMiddle(), cat.getRight().orElse(null)))
+            .<Result<Set<Evaluation>>>reduce(
+                Result.of(new HashSet<>()),
+                (res, cat) -> res.require(cat::isPresent, cat::getError)
+                                 .peek(list -> list.add(cat.getValue())),
+                (res1, res2) -> res1.require(res2::isPresent, res2::getError)
+                                    .peek(list -> list.addAll(res2.orElse(HashSet::new))))
+            .flatMap(cat -> {
+                BatchEvaluationBuilder builder = new BatchEvaluationBuilder();
+                newBatch.getNotes().ifPresent(builder::addNotes);
+                newBatch.getReviewer().ifPresent(builder::addReviewer);
+                return new BatchEvaluationBuilder().build(newBatch.getBatchEvaluationType(), cat)
+                                                   .map(eval -> utils.getBatchById(batchID).map(b -> b.setEvaluation(eval)));
+            })
+            .toEmpty();
+    }
+    @Override
+    public Result<Optional<BatchEvaluationDetailViewModel>> getBatchEvaluation(final int batchID) {
+        return utils.getBatchById(batchID).map(batch -> batch.getEvaluation().map(BatchEvaluationDetailViewModel::new));
     }
 }
