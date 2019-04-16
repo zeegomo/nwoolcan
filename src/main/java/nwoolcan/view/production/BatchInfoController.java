555e5e811015c2b600bb494963cf4b3ee05d8364
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-16 20:02:01 +0200

    inject data into view

--- src/main/java/nwoolcan/view/production/BatchInfoController.java
+++ src/main/java/nwoolcan/view/production/BatchInfoController.java
@@ -2,18 +2,25 @@ package nwoolcan.view.production;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
 import nwoolcan.controller.Controller;
 import nwoolcan.view.InitializableController;
 import nwoolcan.view.ViewManager;
 import nwoolcan.view.subview.SubView;
 import nwoolcan.view.subview.SubViewController;
 import nwoolcan.viewmodel.brewery.production.batch.BatchInfoViewModel;
+import nwoolcan.viewmodel.brewery.production.step.ParameterViewModel;
 
 /**
  * Controller for Batch Info view.
  */
 @SuppressWarnings("NullAway")
 public class BatchInfoController extends SubViewController implements InitializableController<BatchInfoViewModel> {
+    private static final double WRAP = 1.2;
+    private static final int DATA_NUMBER = 6;
+    @FXML
+    private SubView batchInfoSubView;
     @FXML
     private Label nameLabel;
     @FXML
@@ -32,13 +39,15 @@ public class BatchInfoController extends SubViewController implements Initializa
     private Label ibuLabel;
     @FXML
     private Label batchSizeLabel;
+    @FXML
+    private GridPane statsGridPane;
     /**
      * Creates itself and gets injected.
      *
      * @param controller  injected controller.
      * @param viewManager injected view manager.
      */
-    protected BatchInfoController(final Controller controller, final ViewManager viewManager) {
+    public BatchInfoController(final Controller controller, final ViewManager viewManager) {
         super(controller, viewManager);
     }
 
@@ -47,11 +56,21 @@ public class BatchInfoController extends SubViewController implements Initializa
         this.nameLabel.setText(data.getBeerName());
         this.styleLabel.setText(data.getBeerStyle());
         this.styleCategoryLabel.setText(data.getBeerStyleCategory().orElse(""));
-        this.ogLabel.setText(data.ge);
+        data.getOg().ifPresent(val -> this.ogLabel.setText(val.getValueRepresentation()));
+        data.getFg().ifPresent(val -> this.fgLabel.setText(val.getValueRepresentation()));
+        data.getAbv().ifPresent(val -> this.abvLabel.setText(val.getValueRepresentation()));
+        data.getEbc().ifPresent(val -> this.ebcLabel.setText(val.getValueRepresentation()));
+        data.getIbu().ifPresent(val -> this.ibuLabel.setText(val.getValueRepresentation()));
+        this.fgLabel.setText("1.020");
+        this.ogLabel.setText("1.080");
+        this.abvLabel.setText("11%");
+        this.ebcLabel.setText("23");
+        this.ibuLabel.setText("11");
+        this.batchSizeLabel.setText(data.getBatchSize().toString());
     }
 
     @Override
     protected SubView getSubView() {
-        return null;
+        return this.batchInfoSubView;
     }
 }
