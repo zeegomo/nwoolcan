6be5f5fa20080e2fc9e84d317f54a948129e8034
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-04-16 14:32:14 +0200

    remove unused string in breweryImpl

--- src/main/java/nwoolcan/model/brewery/BreweryImpl.java
+++ src/main/java/nwoolcan/model/brewery/BreweryImpl.java
@@ -22,10 +22,6 @@ import java.util.stream.Collectors;
  */
 public final class BreweryImpl implements Brewery {
 
-    private static final String CANNOT_STOCK_NOT_ENDED_BATCH_MESSGE = "Cannot stock a non ended batch.";
-    private static final String ALREADY_STOCKED_BATCH_MESSAGE = "The batch is already stocked.";
-    private static final String WRONG_ARTICLE_UNIT_OF_MEASURE_MESSAGE = "Article unit of measure and batch current size unit of measure do not match.";
-
     @Nullable private String breweryName;
     @Nullable private String ownerName;
     private final Warehouse warehouse = new WarehouseImpl();
