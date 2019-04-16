283819a983e945e915d1a7bb8783dedcbabf92c5
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-16 23:22:50 +0200

    use new viewmanager method to get controller

--- src/main/java/nwoolcan/view/review/EvaluationInputController.java
+++ src/main/java/nwoolcan/view/review/EvaluationInputController.java
@@ -6,30 +6,27 @@ import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
 import nwoolcan.controller.Controller;
 import nwoolcan.model.brewery.batch.review.EvaluationType;
 import nwoolcan.utils.Results;
 import nwoolcan.view.InitializableController;
 import nwoolcan.view.ViewManager;
-import nwoolcan.view.ViewType;
+import nwoolcan.view.subview.SubView;
+import nwoolcan.view.subview.SubViewController;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
-
-import java.io.IOException;
 import java.util.Optional;
 
 /**
  * Controller for single evaluation input displayed in {@link NewBatchEvaluationController}.
  */
 @SuppressWarnings("NullAway")
-public final class EvaluationInputController extends AnchorPane implements InitializableController<EvaluationType> {
+public final class EvaluationInputController extends SubViewController implements InitializableController<EvaluationType> {
     private static final String DEFAULT_TEXT = "-fx-text-fill: black;";
     private static final String ERROR_TEXT = "-fx-text-fill: red;";
     @FXML
@@ -40,9 +37,8 @@ public final class EvaluationInputController extends AnchorPane implements Initi
     private TextArea notes;
     @FXML
     private TitledPane title;
-
-    private final Controller controller;
-    private final ViewManager viewManager;
+    @FXML
+    private SubView evaluationInputSubView;
 
     private final BooleanProperty validityProperty = new SimpleBooleanProperty(false);
     /**
@@ -52,16 +48,7 @@ public final class EvaluationInputController extends AnchorPane implements Initi
      * @param viewManager injected view manager.
      */
     public EvaluationInputController(final Controller controller, final ViewManager viewManager) {
-        this.controller = controller;
-        this.viewManager = viewManager;
-        FXMLLoader loader = new FXMLLoader(ViewType.class.getResource("evaluation_type.fxml"));
-        loader.setRoot(this);
-        loader.setController(this);
-        try {
-            loader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
+        super(controller, viewManager);
     }
 
     @Override
@@ -71,7 +58,7 @@ public final class EvaluationInputController extends AnchorPane implements Initi
             if (unfocus) {
                 Results.ofChecked(() -> Integer.parseInt(this.score.getText()))
                        .flatMap(parsedScore ->
-                           this.controller
+                           this.getController()
                                .getBatchController()
                                .checkEvaluation(Triple.of(data, parsedScore, Optional.of(this.notes.textProperty().get())))
                        )
@@ -91,14 +78,23 @@ public final class EvaluationInputController extends AnchorPane implements Initi
     }
 
     /**
-     * Returns properties of this controller.
-     * @return properties of this controller.
+     * Returns input properties of this controller.
+     * @return input properties of this controller.
      */
     public ReadOnlyObjectWrapper<Pair<ReadOnlyStringProperty, ReadOnlyStringProperty>> getInputProperty() {
         return new ReadOnlyObjectWrapper<>(Pair.of(this.score.textProperty(), this.notes.textProperty()));
     }
+
+    /**
+     * Returns validity of this controller input.
+     * @return validity of this controller input.
+     */
     public ReadOnlyBooleanProperty getInputValidityProperty() {
         return ReadOnlyBooleanProperty.readOnlyBooleanProperty(validityProperty);
     }
 
+    @Override
+    protected SubView getSubView() {
+        return this.evaluationInputSubView;
+    }
 }
