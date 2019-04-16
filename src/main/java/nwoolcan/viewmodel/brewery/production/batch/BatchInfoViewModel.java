8ce5eac1ee165a25ac2820a3f2fa4e6f0b6fbf0e
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-16 14:27:33 +0200

    import ParameterViewModel

--- src/main/java/nwoolcan/viewmodel/brewery/production/batch/BatchInfoViewModel.java
+++ src/main/java/nwoolcan/viewmodel/brewery/production/batch/BatchInfoViewModel.java
@@ -1,11 +1,16 @@
 package nwoolcan.viewmodel.brewery.production.batch;
 
 import nwoolcan.model.brewery.batch.BatchInfo;
+import nwoolcan.model.brewery.batch.misc.WaterMeasurement;
+import nwoolcan.viewmodel.brewery.production.step.ParameterViewModel;
 import nwoolcan.viewmodel.brewery.utils.QuantityViewModel;
 import nwoolcan.viewmodel.brewery.warehouse.article.IngredientArticleViewModel;
 import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,7 +26,7 @@ public class BatchInfoViewModel {
     private final String beerStyleCategory;
     private final String batchMethod;
     private final QuantityViewModel batchSize;
-    /*@Nullable
+    @Nullable
     private final ParameterViewModel og;
     @Nullable
     private final ParameterViewModel fg;
@@ -32,10 +37,8 @@ public class BatchInfoViewModel {
     @Nullable
     private final ParameterViewModel ibu;
     @Nullable
-    private final ParameterViewModel fg;
-    @Nullable
     private final List<ParameterViewModel> waterMeasurements;
-    */
+
     private final List<Pair<IngredientArticleViewModel, Double>> ingredients;
 
     public BatchInfoViewModel(final BatchInfo data) {
@@ -48,6 +51,18 @@ public class BatchInfoViewModel {
                                .stream()
                                .map(pair -> Pair.of(new IngredientArticleViewModel(pair.getLeft()), pair.getRight()))
                                .collect(Collectors.toList());
+        this.waterMeasurements = data.getWaterMeasurements().map(mes -> Arrays.stream(WaterMeasurement.Element.values())
+                                                                              .map(mes::getMeasurement)
+                                                                              .filter(Optional::isPresent)
+                                                                              .map(Optional::get)
+                                                                              .map(ParameterViewModel::new)
+                                                                              .collect(Collectors.toList()))
+                                     .orElse(null);
+        this.abv = data.getAbv().map(ParameterViewModel::new).orElse(null);
+        this.ebc = data.getEbc().map(ParameterViewModel::new).orElse(null);
+        this.ibu = data.getIbu().map(ParameterViewModel::new).orElse(null);
+        this.fg = data.getFg().map(ParameterViewModel::new).orElse(null);
+        this.og = data.getOg().map(ParameterViewModel::new).orElse(null);
     }
 
     /**
