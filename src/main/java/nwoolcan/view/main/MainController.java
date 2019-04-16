925883a56449671f5052ae830d7334956d0c036d
Giacomo Pasini <g.pasini98@gmail.com>
2019-04-16 23:28:14 +0200

    remove dummy controls for review debug

--- src/main/java/nwoolcan/view/main/MainController.java
+++ src/main/java/nwoolcan/view/main/MainController.java
@@ -3,54 +3,16 @@ package nwoolcan.view.main;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import nwoolcan.model.brewery.batch.review.BatchEvaluation;
-import nwoolcan.model.brewery.batch.review.BatchEvaluationBuilder;
-import nwoolcan.model.brewery.batch.review.BatchEvaluationType;
-import nwoolcan.model.brewery.batch.review.Evaluation;
-import nwoolcan.model.brewery.batch.review.EvaluationFactory;
-import nwoolcan.model.brewery.batch.review.types.BJCPBatchEvaluationType;
-import nwoolcan.utils.Result;
 import nwoolcan.controller.Controller;
 import nwoolcan.view.AbstractViewController;
 import nwoolcan.view.ViewManager;
 import nwoolcan.view.ViewType;
 import nwoolcan.view.subview.SubViewContainer;
-import nwoolcan.viewmodel.brewery.production.batch.review.BatchEvaluationDetailViewModel;
-
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 /**
  * Handles the Main view.
  */
 @SuppressWarnings("NullAway") // TODO
 public final class MainController extends AbstractViewController {
-
-    private final BatchEvaluationType bjcpType = BatchEvaluationBuilder.getAvailableBatchEvaluationTypes()
-                                                                       .getValue()
-                                                                       .stream()
-                                                                       .filter(s -> s.getClass().equals(BJCPBatchEvaluationType.class))
-                                                                       .findAny().get();
-
-    Set<Evaluation> evals = Stream.<Result<Evaluation>>builder()
-        .add(EvaluationFactory.create(BJCPBatchEvaluationType.BJCPCategories.AROMA, 10))
-        .add(EvaluationFactory.create(BJCPBatchEvaluationType.BJCPCategories.APPEARANCE, 3))
-        .add(EvaluationFactory.create(BJCPBatchEvaluationType.BJCPCategories.FLAVOR, BJCPBatchEvaluationType.BJCPCategories.FLAVOR.getMaxScore()))
-        .add(EvaluationFactory.create(BJCPBatchEvaluationType.BJCPCategories.MOUTHFEEL, 4))
-        .add(EvaluationFactory.create(BJCPBatchEvaluationType.BJCPCategories.OVERALL_IMPRESSION, 10))
-        .build()
-        .filter(Result::isPresent)
-        .map(Result::getValue)
-        .collect(Collectors.toSet());
-
-    BatchEvaluationBuilder builder = new BatchEvaluationBuilder();
-
-    BatchEvaluation bjcp = builder.addReviewer("Andrea")
-                                          .addNotes("Very goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery goodVery good")
-                                          .build(bjcpType, evals)
-                                          .getValue();
 
     @FXML
     private SubViewContainer contentPane;
@@ -89,13 +51,7 @@ public final class MainController extends AbstractViewController {
     public void menuViewProductionClick(final ActionEvent event) {
         this.getViewManager().getView(ViewType.PRODUCTION, this.getController().getProductionViewModel()).peek(view -> this.contentPane.substitute(view));
     }
-    /**
-     * Shows the Warehouse view.
-     * @param event The occurred event
-     */
-    public void menuViewEvaluationClick(final ActionEvent event) {
-        this.getViewManager().getView(ViewType.BATCHEVALUATIONDETAIL, new BatchEvaluationDetailViewModel(bjcp)).peek(view -> this.contentPane.substitute(view));
-    }
+
     /**
      * Shows the Articles view.
      * @param event The occurred event
