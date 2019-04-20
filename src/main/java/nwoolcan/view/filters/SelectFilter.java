782a01826e57b2829f819745a67072aa251251ce
Carlovan <giulio.carlassare@gmail.com>
2019-04-20 09:45:40 +0200

    Added SelectFilter (aka combobox)

---
+++ src/main/java/nwoolcan/view/filters/SelectFilter.java
@@ -0,0 +1,37 @@
+package nwoolcan.view.filters;
+
+import javafx.beans.NamedArg;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
+/**
+ * Filter with a date value.
+ * @param <T> the type of the items inside the combobox.
+ */
+public class SelectFilter<T> extends VBox {
+    private final ComboBox<T> field = new ComboBox<>();
+
+    /**
+     * Create a new TextFilter.
+     * @param title the title of the filter.
+     * @param placeholder the text tu put inside the combobox when nothing is selected.
+     * @param items the items tu put inside the combobox.
+     */
+    public SelectFilter(@NamedArg("title") final String title, @NamedArg("placeholder") final String placeholder, @NamedArg("items") final List<T> items) {
+        this.getChildren().add(new Label(title));
+        this.getChildren().add(this.field);
+        this.field.getItems().addAll(items);
+        this.field.setPromptText(placeholder);
+    }
+
+    /**
+     * Returns the filter's value.
+     * @return the filter's value.
+     */
+    public T getValue() {
+        return this.field.getValue();
+    }
+}
