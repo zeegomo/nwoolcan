f2d6f343de5d4beb65796823a8d5be50a487a694
Samuele Ceroni <samuele.ceroni@studio.unibo.it>
2019-04-21 18:26:25 +0200

    associate initial step to batch method in enum

--- src/main/java/nwoolcan/model/brewery/batch/BatchMethod.java
+++ src/main/java/nwoolcan/model/brewery/batch/BatchMethod.java
@@ -1,5 +1,7 @@
 package nwoolcan.model.brewery.batch;
 
+import nwoolcan.model.brewery.batch.step.StepType;
+import nwoolcan.model.brewery.batch.step.StepTypeEnum;
 import nwoolcan.utils.StringUtils;
 
 /**
@@ -9,20 +11,22 @@ public enum BatchMethod {
     /**
      * All grain.
      */
-    ALL_GRAIN("All grain"),
+    ALL_GRAIN("All grain", StepTypeEnum.MASHING),
     /**
      * Partial mash.
      */
-    PARTIAL_MASH("Partial Mash"),
+    PARTIAL_MASH("Partial Mash", StepTypeEnum.MASHING),
     /**
      * Extract.
      */
-    EXTRACT("Exctract");
+    EXTRACT("Exctract", StepTypeEnum.BOILING);
 
     private final String name;
+    private final StepTypeEnum initialStep;
 
-    BatchMethod(final String name) {
+    BatchMethod(final String name, final StepTypeEnum initialStep) {
         this.name = name;
+        this.initialStep = initialStep;
     }
     /**
      * Return a {@link String} representation of the name of the method.
@@ -30,6 +34,14 @@ public enum BatchMethod {
      */
     public String getName() {
         return this.name;
+    }
+
+    /**
+     * Return the initial {@link StepType} of the {@link BatchMethod}.
+     * @return the initial {@link StepType} of the {@link BatchMethod}.
+     */
+    public StepType getInitialStep() {
+        return this.initialStep;
     }
 
     @Override
